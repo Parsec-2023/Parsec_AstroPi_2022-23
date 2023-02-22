89fb7743beff2d72ec2000769827d64b78abf09e
Federi0411-0684 <113929339+Federi0411-0684@users.noreply.github.com>
2023-02-22 15:08:05 +0100

    Final version 1.0
    scalingFactor = 0.3
    Fixed random exif altitude

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+from PIL import Image
 from orbit import ISS, ephemeris
 from skyfield.api import load
 from pathlib import Path
@@ -20,7 +21,7 @@ sense = SenseHat()
 baseFolder = Path(__file__).parent.resolve()
 
 # open log file or create it if it does not exist
-logfile = open(baseFolder / "log.txt", "a", encoding = "utf-8")
+logfile = open(str(baseFolder / "log.txt"), "a", encoding = "utf-8")
 
 def main():
     # csv file header
@@ -47,11 +48,20 @@ def main():
         "Humidity[%]",
     ]
 
+    # initialise the path to the folder that will contain the pictures
+    picsFolder = str(baseFolder) + "/Pictures"
+    try:
+        # and create it if it does not exist
+        if (not os.path.exists(picsFolder)):
+            os.makedirs(picsFolder)
+    except Exception as e:
+        log("FATAL ERROR - unable to find or create Pictures folder: " + str(e))
+
     # open csv file or create it if it does not exist
     try:
-        datafile = open(baseFolder / "data.csv", "r+", encoding = "utf-8")
+        datafile = open(str(baseFolder / "data.csv"), "r+", encoding = "utf-8")
     except:
-        datafile = open(baseFolder / "data.csv", "a+", encoding = "utf-8")
+        datafile = open(str(baseFolder / "data.csv"), "a+", encoding = "utf-8")
         log("File \"data.csv\" not found. New copy created")
 
     datawriter = csv.writer(datafile)
@@ -65,31 +75,41 @@ def main():
         # initialise camera
         camera = PiCamera()
         camera.resolution = (4056, 3040)
-        camera.framerate = 15
+        camera.framerate = 24
         log("Camera initialised")
     except Exception as e:
         log("Error initialising camera: " + str(e))
 
-    # reset current time and picture timer
+    # initialise time and picture timer
     now = datetime.now()
     picTime = datetime.now()
     
-    # picture counter
-    n = 0
+    # initialise picture counters
+    n = 0 # total number of cycles
+    p = 0 # number of pictures taken
+
+    # initialise the size of the Pictures folder (...<baseFolder>/Pictures)
+    picFolderSize = 0
+
+    # set the initial interval for taking pictures to 3 seconds
+    interval = 3
 
     # run the loop for 2 hours and 55 minutes after start time
-    while (now < (startTime + timedelta(hours = 2, minutes = 55))):
+    while (now < (startTime + timedelta(hours = 2, minutes = 57, seconds = 30))):
 
-        #-start counting the time
-        s = datetime.now()
+        # start counting the time for each picture
+        picDeltaTime = datetime.now()
 
-        # take a picture and save data every >=15 seconds
-        if (now >= (picTime + timedelta(seconds = 15))):
+        # take a picture and save data every 'interval' seconds, if interval is bigger than 3
+        # (we have calculated that if we take a picture every 3 seconds, the data limit of 3GB should not be exceeded)
+        if (now >= (picTime + timedelta(seconds = (int(interval) if (interval >= 3) else 3)))):
             # if it is nighttime, do not take the picture
             if (ISS.at(load.timescale().now()).is_sunlit(ephemeris)):
                 try:
-                    # set picture path
-                    picPath = str(baseFolder) + "/Pictures/" + "image_" + str(n) + ".jpg"
+                    # temporary picture path
+                    tmpPicPath = picsFolder + "/tmpImage.jpg"
+                    # set the path where the final image will be saved
+                    picPath = picsFolder + "/image_" + str(n) + ".jpg"
 
                     # locate the ISS
                     altitude, latitude, longitude = getISSPos()
@@ -99,42 +119,81 @@ def main():
                     west, exifLongitude = convertToExif(longitude)
 
                     # save the location in the metadata of the picture
-                    camera.exif_tags['GPS.GPSAltitude'] = altitude
+                    camera.exif_tags['GPS.GPSAltitude'] = (str(int(altitude)) + "/1")
                     camera.exif_tags['GPS.GPSLatitude'] = exifLatitude
                     camera.exif_tags['GPS.GPSLatitudeRef'] = ("S" if south else "N")
                     camera.exif_tags['GPS.GPSLongitude'] = exifLongitude
                     camera.exif_tags['GPS.GPSLongitudeRef'] = ("W" if west else "E")
 
-                    # take a picture at full resolution
-                    camera.capture(picPath)
+                    # take a picture at full resolution and save it as temporary
+                    camera.capture(tmpPicPath, quality = 100)
+                    print("Tmp picture saved at: " + tmpPicPath)
 
                     # open the picture as an OpenCV image
-                    image = cv2.imread(picPath)
+                    image = cv2.imread(tmpPicPath)
+                    print("Tmp picture opened")
 
                     # scale down the image to make the following operations faster
-                    scalingFactor = 0.4
+                    scalingFactor = 0.3
                     scaledImage = cv2.resize(image, None, fx = scalingFactor, fy = scalingFactor)
+                    print("Picture resized")
 
                     # perform image segmentation on the scaled picture
                     segmented = segmentation(scaledImage)
+                    print("Picture segmented")
 
-                    # if the picture is relevant to our research (there is enough land)
+                    # if the picture is relevant to our research (there is enough land)...
                     score = evaluate(segmented)
+                    print("Score: " + str(score))
                     if (score >= 2.5):
-                        # crop the image to the window of the ISS to save storage space
+                        # crop the original image to the window of the ISS to save storage space
                         image = cropCircle(scaledImage, image, scalingFactor)
+                        print("Picture cropped")
 
-                        # save the cropped image
+                        # save the cropped image with its final name
                         cv2.imwrite(picPath, image)
+                        print("Picture saved at: " + picPath)
+
+                        # save the exif data to the final image
+                        try:
+                            # open the temporary image with PIL
+                            tmpImage = Image.open(tmpPicPath)
+                            # get its exif data
+                            exifData = tmpImage.info["exif"]
+                            # open the cropped image with PIL
+                            finalImage = Image.open(picPath)
+                            # save the exif data to the final image
+                            finalImage.save(picPath, exif = exifData)
+                        except Exception as e:
+                            log("Unable to save EXIF data: " + str(e))
+
+                        # increment the number of pictures
+                        p += 1
+                        # increment the size of the Pictures folder
+                        picFolderSize += os.path.getsize(picPath)
+
+                        # update the minimum time interval for taking pictures according to the remaining time and remaining storage space:
+                        # the remaining space is the total space (we went for 2.975GB to leave some wiggle room for safety) minus the current space taken
+                        remainingSpace = 2975000000 - picFolderSize
+                        # the remaining time is the initial time plus almost three hours minus the current time
+                        remainingTime = (startTime + timedelta(hours = 2, minutes = 57, seconds = 30) - datetime.now()).seconds
+                        # given 'remainingSpace' bytes left and 'remainingTime' seconds to save an 'averageSpace' amount of bytes every 'interval' seconds, the following proportion applies:
+                        # interval : averageSpace = remainingTime : remainingSpace
+                        # therefore -> interval = averageSpace * remainingTime / remainingSpace
+                        # the average space of one picture is calculated by dividing the total size of the Pictures folder by the number of pictures it contains
+                        interval = ((picFolderSize / p) * remainingTime / remainingSpace)
 
                         # log the coordinates where the pic was taken
                         log("Picture " + "\"image_" + str(n) + ".jpg\"" + " taken at: (" + str(latitude) + ", " + str(longitude) + ") [score: " + str(round(score, 3)) + "]")
+
+                        # log the new time interval
+                        log("Time interval: " + str(interval))
                     else:
                         log("Picture not taken - Not relevant")
-
-                    #-print time taken
-                    print(datetime.now() - s)
-
+                    
+                    # print time taken
+                    log("Time taken: " + str(datetime.now() - picDeltaTime))
+                    
                 except Exception as e:
                     log("Error taking a picture: " + str(e))
             else:
@@ -155,6 +214,17 @@ def main():
 
         # update the current time
         now = datetime.now()
+
+        # if the size of the folder exceeds 2.75GB, stop the program
+        if (picFolderSize >= 2750000000):
+            elapsedTime = str(int((now - startTime).seconds))
+            log("Program aborted after " + elapsedTime + "s: size limit exceeded")
+            break
+    
+    # log the final time in case the program ended correctly after 2h:55m
+    totalTime = datetime.now() - startTime
+    if (totalTime >= timedelta(hours = 2, minutes = 57, seconds = 30)):
+        log("Program successfully terminated after " + str(totalTime.seconds) + "s")
 
     # close camera and files
     camera.close()
@@ -279,9 +349,9 @@ def evaluate(im):
     totalPixels = im.shape[0] * im.shape[1]
     
     # count the number of green pixels, excluding the white pixels
-    greenPixels = len(np.where((g != b) & (g != r))[0])
+    greenPixels = np.sum((g != b) & (g != r))
     # count the number of red pixels, excluding the white pixels
-    redPixels = len(np.where((r != b) & (r != g))[0])
+    redPixels = np.sum((r != b) & (r != g))
 
     # calculate the percentage of green pixels
     percentageGreen = greenPixels / totalPixels * 100
@@ -297,6 +367,9 @@ def convertToExif(angle):
     # get the sign
     sign = angle
 
+    # make the angle positive
+    angle = abs(angle)
+
     # get the degrees
     degrees = int(angle)
   
@@ -311,7 +384,7 @@ def convertToExif(angle):
     seconds = int(angle)
 
     # format the angle as a string
-    exifAngle = f"{degrees:.0f}/1,{minutes:.0f}/1,{seconds*1000000:.0f}/1000000"
+    exifAngle = f"{degrees:.0f}/1,{minutes:.0f}/1,{seconds*1000:.0f}/1000"
     
     return (sign < 0), exifAngle
 
@@ -329,9 +402,13 @@ def getTime():
 
 def log(msg):
     # appends a line that indicates the current time and date and the message msg to the file f 
-    logfile.write("[" + getDate() + "," + getTime() + "] " + msg + "\n")
+    message = ("[" + getDate() + "," + getTime() + "] " + msg + "\n")
+    logfile.write(message)
     logfile.flush()
     os.fsync(logfile)
+
+    # print the message to console
+    print(message)
 
 def cropCircle(scaledIm, im, scalingFactor):
     # get the size of the scaled image
@@ -358,6 +435,9 @@ def cropCircle(scaledIm, im, scalingFactor):
             # crop the image to fit the circle that has been found
             im = cv2.getRectSubPix(im, size, (int(xCentre + 5), int(yCentre)))
             break
+    # return the original image object
+    # if it has been cropped, the object itself is the cropped image
+    # otherwise, if it has not been cropped, the image is the same
     return im
 
 def colourise(im, r, g, b):
